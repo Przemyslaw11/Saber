b9094f4ba5f33f197151615fb754aaffc5d2d4e5
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2024-05-10 11:17:01 +0200

    Implemented data conversion algorithms

--- saber/utils/algorithms.py
+++ saber/utils/algorithms.py
@@ -44,6 +44,34 @@ class Polynomial():
     def __getitem__(self, key) -> int:
         assert key < self.n, "Index out of bounds. Index should be strictly less than the degree of the polynomial."
         return self.coeffs[key]
+    
+    def __str__(self) -> str:
+        return str(self.np_poly1d)
+
+
+def int2bytes(n: int, byte_length: int) -> bytes:
+    return n.to_bytes(byte_length, 'big')
+
+def int2bits(n: int, bit_length: int) -> np.ndarray:
+    result = np.zeros(bit_length, dtype=np.uint8)
+    unpacked = np.unpackbits(np.frombuffer(int2bytes(n, n.bit_length()), dtype=np.uint8))[-bit_length:]
+
+    if unpacked.size > 0:
+        result[-len(unpacked):] = unpacked
+    
+    return result
+
+def bytes2bits(b: bytes) -> np.ndarray:
+    return np.unpackbits(np.frombuffer(b, dtype=np.uint8))
+
+def bytes2int(b: bytes) -> int:
+    return int.from_bytes(b, 'big')
+
+def bits2int(bits: np.ndarray) -> int:
+    return np.sum(bits * 2**np.arange(len(bits) - 1, -1, -1))
+
+def bits2bytes(bits: np.ndarray) -> bytes:
+    return np.packbits(bits).tobytes()
 
 
 def shiftleft(pin: Polynomial, s: int) -> Polynomial:
@@ -62,3 +90,41 @@ def shiftright(pin: Polynomial, s: int) -> Polynomial:
         pin_coeffs[i] = pin_coeffs[i] >> s
     
     return Polynomial(pin_coeffs, N)
+
+
+def bs2pol(bs: bytes) -> Polynomial:
+    assert len(bs) % 32 == 0, "The length of the byte string should be a multiple of 256 bits."
+    k = len(bs) // 32
+    N = 2**k
+    bit_string = bytes2bits(bs)
+    coeffs = np.zeros(256, dtype=int)
+    for i in range(255, -1, -1):
+        coeffs[255 - i] = bits2int(bit_string[i*k:(i + 1)*k])
+    return Polynomial(list(coeffs), N)
+
+
+def pol2bs(pin: Polynomial) -> bytes:
+    N = pin.N
+    k = int(np.log2(N))
+    bit_string = np.zeros(k * 256, dtype=int)
+    for i in range(256):
+        bits = int2bits(int(pin[255 - i]), k)
+        bit_string[i*k:(i + 1)*k] = bits
+    return bits2bytes(bit_string)
+
+
+def bs2polvec(bs: bytes, l: int) -> List[Polynomial]:
+    assert len(bs) % 32 == 0, "The length of the byte string should be a multiple of 256 bits."
+    k = (len(bs) // 32) // l
+    v = list()
+    for i in range(l - 1, -1, -1):
+        v.append(bs2pol(bs[i*k*32:(i + 1)*k*32]))
+    return v
+
+
+def polvec2bs(v: List[Polynomial]) -> bytes:
+    l = len(v)
+    bss = list()
+    for i in range(l - 1, -1, -1):
+        bss.append(pol2bs(v[i]))
+    return b''.join(bss)
