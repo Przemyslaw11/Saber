166dd01c032c78e02beb66d5b78e057075dd6280
Przemyslaw11 <przpyra@student.agh.edu.pl>
2024-05-13 00:11:10 +0200

    Add KeyGen function to KEM class

--- saber/kem.py
+++ saber/kem.py
@@ -1,5 +1,7 @@
+from saber.utils.binary_utils import int2bytes
 from utils.algorithms import *
-
+from saber.pke import PKE
+from typing import Tuple
 
 class KEM:
     
@@ -7,12 +9,21 @@ class KEM:
         self.KeyGen = self.KeyGen
         self.Encaps = self.Encaps
         self.Decaps = self.Decaps
-    
-    def KeyGen(self):
-        pass
+        self.constants = constants
 
-    def Encaps(self):
-        pass
+    def KeyGen(self) -> Tuple[bytes, bytes]:
+        '''Returns the public key and the secret key in two separate byte arrays of size
+SABER_PUBLICKEYBYTES and SABER_SECRETKEYBYTES respectively. Function from the [SABER](https://www.esat.kuleuven.be/cosic/pqcrypto/saber/files/saberspecround3.pdf#page=29.49) specification.'''
+        PublicKeycpa, SecretKeycpa = PKE.KeyGen()
 
-    def Decaps(self):
-        pass
+        hash_pk = sha3_256(PublicKeycpa).digest()
+        hash_input = hash_pk + PublicKeycpa[:self.constants["SABER_INDCPA_PUBLICKEYBYTES"]]
+        hash_pk = sha3_256(hash_input).digest()
+
+        z = randombytes(self.constants["SABER_KEYBYTES"])
+
+        SecretKeycca = z + hash_pk + PublicKeycpa + SecretKeycpa
+
+        PublicKeycca = PublicKeycpa
+
+        return PublicKeycca, SecretKeycca
