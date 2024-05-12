9ca129c4a161c6825ab4a22106e9826e1929d022
Przemyslaw11 <przpyra@student.agh.edu.pl>
2024-05-13 00:11:38 +0200

    Add Encaps function to KEM class

--- saber/kem.py
+++ saber/kem.py
@@ -27,3 +27,29 @@ SABER_PUBLICKEYBYTES and SABER_SECRETKEYBYTES respectively. Function from the [S
         PublicKeycca = PublicKeycpa
 
         return PublicKeycca, SecretKeycca
+
+    def Encaps(self, PublicKeycca: bytes) -> Tuple[bytes, bytes]:
+        '''Generates a session key and the ciphertext corresponding the k.  Function from the [SABER](https://www.esat.kuleuven.be/cosic/pqcrypto/saber/files/saberspecround3.pdf#page=29.49) specification.'''
+        m = randombytes(self.constants["SABER_KEYBYTES"])
+        m = sha3_256(m).digest()
+
+        hash_pk = sha3_256(PublicKeycca).digest()
+        hash_input = hash_pk + PublicKeycca[:self.constants["SABER_INDCPA_PUBLICKEYBYTES"]]
+        hash_pk = sha3_256(hash_input).digest()
+
+        buf = hash_pk + m
+
+        rk = sha3_512(buf).digest()
+
+        r, k = rk[:self.constants["SABER_KEYBYTES"]], rk[self.constants["SABER_KEYBYTES"]:]
+
+        CipherTextcca = PKE.Enc(m, r, PublicKeycca)
+
+        hash_input_2 = r + CipherTextcca + int2bytes(self.constants["SABER_BYTES_CCA_DEC"], 4)
+        r_prime = sha3_256(hash_input_2).digest()
+
+        rk_prime = r_prime + k
+
+        SessionKeycca = sha3_256(rk_prime).digest()
+
+        return SessionKeycca, CipherTextcca
